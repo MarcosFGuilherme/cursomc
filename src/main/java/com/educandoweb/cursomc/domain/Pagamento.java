f6c353083fcf3b5a3cfce4d683c44a1045300918
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-11-14 17:37:48 -0300

    18-Inserindo pedido

--- src/main/java/com/educandoweb/cursomc/domain/Pagamento.java
+++ src/main/java/com/educandoweb/cursomc/domain/Pagamento.java
@@ -12,9 +12,11 @@ import javax.persistence.OneToOne;
 
 import com.educandoweb.cursomc.domain.enums.EstadoPagamento;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
 public abstract class Pagamento implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -51,6 +53,14 @@ public abstract class Pagamento implements Serializable{
 
 	public void setEstado(EstadoPagamento estado) {
 		this.estado = estado.getCod();
+	}
+	
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
 	}
 
 	@Override
