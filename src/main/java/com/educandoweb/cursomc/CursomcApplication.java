f0a5b0e7a8defddb10d0fae8d14ce6b8841e8f62
Marcos F S Guilherme <marcosfs.guilherme@gmail.com>
2020-01-02 10:29:28 -0300

    37-Salvando primeiro arquivo no S3

--- src/main/java/com/educandoweb/cursomc/CursomcApplication.java
+++ src/main/java/com/educandoweb/cursomc/CursomcApplication.java
@@ -1,12 +1,18 @@
 package com.educandoweb.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.educandoweb.cursomc.services.S3Service;
+
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -14,6 +20,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3service.uploadFile("E:\\Temp\\fotos\\DS3.PNG");
 	}
 }
